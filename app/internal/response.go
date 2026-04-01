f9f911d52d5e415c87987212667ee0fddaebc0a0
AdrianBadea23 <badeaadrian699@gmail.com>
2026-04-01 15:18:43 +0300

    codecrafters submit [skip ci]

--- app/internal/response.go
+++ app/internal/response.go
@@ -14,8 +14,11 @@ const GET = "GET"
 
 const PONG = "+PONG\r\n"
 const OK = "+OK\r\n"
+const NULL_BULK_STRING = "$-1\r\n"
 
 func HandleConnection(conn net.Conn) {
+
+	keyValue := make(map[string]string)
 
 	for {
 		recv := bufio.NewReader(conn)
@@ -33,6 +36,29 @@ func HandleConnection(conn net.Conn) {
 				writer.WriteString(tokens[1])
 				writer.WriteString("\r\n")
 				writer.Flush()
+			}
+
+			if strings.EqualFold(tokens[0], SET) {
+				keyValue[tokens[1]] = tokens[2]
+				writer.WriteString(OK)
+				writer.Flush()
+			}
+
+			if strings.EqualFold(tokens[0], GET) {
+				val, ok := keyValue[tokens[1]]
+
+				if !ok {
+					writer.WriteString(NULL_BULK_STRING)
+					writer.Flush()
+				} else {
+					writer.WriteString("$")
+					writer.WriteString(strconv.Itoa(len(val)))
+					writer.WriteString("\r\n")
+					writer.WriteString(val)
+					writer.WriteString("\r\n")
+					writer.Flush()
+				}
+
 			}
 
 			if strings.EqualFold(tokens[0], PING) {
