7f36cd6b0c2116449623f7be41d0c96f79b1d522
AdrianBadea23 <badeaadrian699@gmail.com>
2026-04-01 14:28:02 +0300

    codecrafters submit [skip ci]

--- app/internal/parser.go
+++ app/internal/parser.go
@@ -14,6 +14,7 @@ func bulkString(reader *bufio.Reader) string {
 	}
 
 	number, _ := reader.ReadString(byte('\r'))
+	number = number[:len(number)-1]
 
 	length, _ := strconv.ParseInt(string(number), 10, 64)
 	reader.ReadByte()
@@ -26,11 +27,19 @@ func bulkString(reader *bufio.Reader) string {
 	return string(buff)
 }
 
+func parseString(input string) string {
+	reader := bufio.NewReader(strings.NewReader(input))
+	reader.ReadByte() // expect +
+	fin, _ := reader.ReadString('\r')
+	return fin[:len(fin)-1]
+}
+
 func arrayParser(input string) []string {
 	reader := bufio.NewReader(strings.NewReader(input))
 	reader.ReadByte() // expect *
 
 	num, _ := reader.ReadString(byte('\r'))
+	num = num[:len(num)-1]
 	numInteger, _ := strconv.Atoi(string(num))
 	args := make([]string, numInteger)
 
--- app/internal/response.go
+++ app/internal/response.go
@@ -4,31 +4,61 @@ import (
 	"bufio"
 	"log"
 	"net"
+	"strconv"
+	"strings"
 )
 
 func HandleConnection(conn net.Conn) {
 
 	for {
-		command, err := bufio.NewReader(conn).ReadString('\n')
+		recv, err := bufio.NewReader(conn).ReadString('\n')
 		if err != nil {
 			log.Println("Error reading fron conn")
 		}
 
-		tokens := arrayParser(command)
-		writer := bufio.NewWriter(conn)
+		var sb strings.Builder
+		sb.WriteString(recv)
 
-		if tokens[0] == "ECHO" {
-			writer.WriteString("$")
-			writer.WriteString(string(len(tokens[1])))
-			writer.WriteString("\r\n")
-			writer.WriteString(tokens[1])
-			writer.WriteString("\r\n")
-			writer.Flush()
+		if recv[0] == '*' {
+			specLen, err := strconv.Atoi(recv[1 : len(recv)-2])
+
+			if err != nil {
+				log.Println("error with conversion")
+			}
+
+			for range specLen {
+				chunk, _ := bufio.NewReader(conn).ReadString('\n')
+				sb.WriteString(chunk)
+			}
 		}
 
-		if tokens[0] == "PING" {
-			writer.WriteString("+PONG\r\n")
-			writer.Flush()
+		command := sb.String()
+		reader := bufio.NewReader(strings.NewReader(command))
+
+		firstToken, _ := reader.ReadByte()
+
+		switch firstToken {
+		case '*':
+			tokens := arrayParser(command)
+			writer := bufio.NewWriter(conn)
+
+			if strings.EqualFold(tokens[0], "ECHO") {
+				writer.WriteString("$")
+				writer.WriteString(string(len(tokens[1])))
+				writer.WriteString("\r\n")
+				writer.WriteString(tokens[1])
+				writer.WriteString("\r\n")
+				writer.Flush()
+			}
+
+		case '+':
+			myString := parseString(command)
+			writer := bufio.NewWriter(conn)
+
+			if strings.EqualFold(myString, "PING") {
+				writer.WriteString("+PONG\r\n")
+				writer.Flush()
+			}
 		}
 
 	}
